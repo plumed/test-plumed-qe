cfcf788a6ccfbb3e571cddf6ddbfdc15f3a6d5f8
Ralf Meyer <meyer.ralf@yahoo.com>
2022-03-12 14:47:39 -0500

    Add first test

---
+++ run_tests.sh
@@ -0,0 +1,24 @@
+cd test_Si
+
+pw.x -plumed < md.in > md.out
+
+for file in *.reference ; do
+  new="${file%.reference}"
+  if test -f "$new" ; then
+    cp "$new" "$new.zfix"
+    DIFFOPT=""
+    out="$(diff $DIFFOPT "$file" "$new.zfix")"
+    test -n "$out" && {
+      echo FAILURE
+      echo "Diff for ${file%.reference}:"
+      echo "$out"
+      exit 1
+    }
+  else
+    echo FAILURE
+    echo FILE $new does not exist
+    exit 1
+  fi
+done
+
+cd ../
