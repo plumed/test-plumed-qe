ade4fd61b705cc9898621c0b4f1744683d811add
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-07-10 10:28:11 +0200

    updated the test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,24 +1,27 @@
-cd test_Si
+for testDir in test_Si test_H2O; do
+  (
+    cd ${testDir} || exit
 
-pw.x -plumed < md.in > md.out
+    pw.x -plumed <md.in >md.out
 
-for file in *.reference ; do
-  new="${file%.reference}"
-  if test -f "$new" ; then
-    cp "$new" "$new.zfix"
-    DIFFOPT=""
-    out="$(diff $DIFFOPT "$file" "$new.zfix")"
-    test -n "$out" && {
-      echo FAILURE
-      echo "Diff for ${file%.reference}:"
-      echo "$out"
-      exit 1
-    }
-  else
-    echo FAILURE
-    echo FILE $new does not exist
-    exit 1
-  fi
+    for file in *.reference; do
+      new="${file%.reference}"
+      if test -f "$new"; then
+        cp "$new" "$new.zfix"
+        DIFFOPT=""
+        out="$(diff $DIFFOPT "$file" "$new.zfix")"
+        test -n "$out" && {
+          echo FAILURE
+          echo "Diff for ${file%.reference}:"
+          echo "$out"
+          exit 1
+        }
+      else
+        echo FAILURE
+        echo FILE $new does not exist
+        exit 1
+      fi
+    done
+
+  )
 done
-
-cd ../
